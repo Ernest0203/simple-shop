a3e8ccde3cb79047d502087f2322ef3a17367df6
Ernest0203 <ernest2332@gmail.com>
2019-01-10 14:37:33 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import './App.css';
+import './css/main.css';
 
 class App extends Component {
   render() {
